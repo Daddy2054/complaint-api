dcd7139d4ec939b89c5a0209aa40cb76b1fd9a63
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-10 20:26:46 +0100

    register,login endpoints

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from resources.routes import api_router
+from db import database
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup: Code that runs before the application starts
+    await database.connect()
+    yield
+    # Shutdown: Code that runs when the application is shutting down
+    await database.disconnect()
 
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(api_router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
---
+++ managers/auth.py
@@ -0,0 +1,56 @@
+from typing import Optional
+#import dotenv
+from fastapi import HTTPException #, Request
+from starlette.requests import Request
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+import jwt
+
+from datetime import datetime, timedelta, timezone
+import os
+from db import database
+from models import user
+
+#dotenv.load_dotenv()
+secret_key = os.getenv('SECRET_KEY')
+
+class AuthManager:
+    @staticmethod
+    def encode_token(user):
+        try:
+
+            payload = {
+                'sub': user["id"],
+                'exp': datetime.now(timezone.utc) + timedelta(days=1),
+                #'iat': datetime.now(datetime.timezone.utc)
+            }
+
+            encoded_jwt = jwt.encode(
+                payload,
+                secret_key,
+                algorithm='HS256'
+            )  
+            return encoded_jwt
+        except Exception as e:
+            raise e 
+        
+class CustomHTTPBearer(HTTPBearer):
+    async def ____call__(
+        self, request: Request
+    ) -> Optional[HTTPAuthorizationCredentials]:
+        res = await super().__call__(request)
+
+        try:
+            payload = jwt.decode(
+                res.credentials, os.getenv("JWT_SECRET"), algorithms=["HS256"]
+            )
+            user_id = payload["sub"]
+            user_data = await database.fetch_one(user.select().where(user.c.id == user_id))
+            request.state.user = user_data
+            return user_data
+
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(401, "Token has expired")
+
+        except jwt.InvalidTokenError:
+            raise HTTPException(401, "Invalid token")
