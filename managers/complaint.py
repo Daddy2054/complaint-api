91436ca115c7a746c22924eb5d8b4be60922f410
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-11 21:00:30 +0100

    approve reject

--- managers/complaint.py
+++ managers/complaint.py
@@ -2,6 +2,7 @@ from models import complaint
 from models.enums import RoleType, State
 from db import database
 
+
 class ComplaintManager:
     @staticmethod
     async def get_complaints(user):
@@ -11,14 +12,30 @@ class ComplaintManager:
         elif user["role"] == RoleType.approver:
             query = query.where(complaint.c.status == State.pending)
         return await database.fetch_all(query)
-    
+
     @staticmethod
-    async def create_complaint(complaint_data,user):
+    async def create_complaint(complaint_data, user):
         complaint_data["complainer_id"] = user["id"]
         # complaint_data["status"] = State.pending
         id_ = await database.execute(complaint.insert().values(**complaint_data))
         return await database.fetch_one(complaint.select().where(complaint.c.id == id_))
-    
+
     @staticmethod
     async def delete_complaint(complaint_id):
         await database.execute(complaint.delete().where(complaint.c.id == complaint_id))
+
+    @staticmethod
+    async def approve(complaint_id):
+        await database.execute(
+            complaint.update()
+            .where(complaint.c.id == complaint_id)
+            .values(status=State.approved)
+        )
+
+    @staticmethod
+    async def reject(complaint_id):
+        await database.execute(
+            complaint.update()
+            .where(complaint.c.id == complaint_id)
+            .values(status=State.rejected)
+        )
--- resources/complaint.py
+++ resources/complaint.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import APIRouter, Depends  # , Request
 from starlette.requests import Request
-from managers.auth import CustomHTTPBearer, is_admin, is_complainer, oauth2_scheme
+from managers.auth import CustomHTTPBearer, is_admin, is_approver, is_complainer, oauth2_scheme
 from managers.complaint import ComplaintManager
 from schemas.request.complaint import ComplaintIn
 from schemas.response.complaint import ComplaintOut
@@ -18,8 +18,6 @@ async def get_complaints(request: Request):
     user = request.state.user
     return await ComplaintManager.get_complaints(user)
 
-
-# request.headers["authorization"]
 @router.post(
     "/complaints/",
     dependencies=[Depends(oauth2_scheme), Depends(is_complainer)],
@@ -29,12 +27,26 @@ async def create_complaint(request: Request, complaint: ComplaintIn):
     user = request.state.user
     return await ComplaintManager.create_complaint(complaint.model_dump(), user)
 
-
 @router.delete(
     "/complaints/{complaint_id}/",
     dependencies=[Depends(oauth2_scheme), Depends(is_admin)],
     status_code=204,
 )
 async def delete_complaint(complaint_id: int):
-    #  user = request.state.user
     return await ComplaintManager.delete_complaint(complaint_id)
+
+@router.put(
+    "/complaints/{complaint_id}/approve/",
+    dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
+    status_code=204,
+)
+async def approve_complaint(complaint_id: int):
+    return await ComplaintManager.approve(complaint_id)
+
+@router.put(
+    "/complaints/{complaint_id}/reject/",
+    dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
+    status_code=204,
+)
+async def reject_complaint(complaint_id: int):
+    return await ComplaintManager.reject(complaint_id)
