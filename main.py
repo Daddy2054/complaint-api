5079f308a1b73515801fcae43e48b5f38bb6c3d4
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-11 21:21:21 +0100

    cors

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import uvicorn
 from resources.routes import api_router
 from db import database
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "http://localhost",
+ 
+]
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup: Code that runs before the application starts
@@ -16,6 +21,13 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
