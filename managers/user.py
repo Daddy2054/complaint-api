056564bdf7716b2e0ffa0509a3b03752ceda4407
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-11 19:47:08 +0100

    hide password

--- managers/user.py
+++ managers/user.py
@@ -33,4 +33,4 @@ class UserManager:
     
     @staticmethod
     async def get_user_by_email(email):
-        return await database.fetch_one(user.select().where(user.c.email == email))
+        return await database.fetch_all(user.select().where(user.c.email == email))
