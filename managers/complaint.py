81857da9f08cbb0abf16752a49012eab3a4d56c6
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-14 22:41:23 +0100

    wise create transfer

--- managers/complaint.py
+++ managers/complaint.py
@@ -1,12 +1,14 @@
 import os
 import uuid
 from constants import TEMP_FILE_FOLDER
-from models import complaint
+from models import complaint, transaction
 from models.enums import RoleType, State
 from db import database
 from services.s3 import S3Service
 from services.ses import SESService
+from services.wise import WiseService
 from utils.helpers import decode_photo
+
 
 s3 = S3Service()
 ses = SESService()
@@ -32,6 +34,12 @@ class ComplaintManager:
         complaint_data["photo_url"] =s3.upload(path, name,extension)
         os.remove(path)
         id_ = await database.execute(complaint.insert().values(**complaint_data))
+        await ComplaintManager.issue_transaction(
+            complaint_data["amount"],
+            f"{user['first_name']} {user['last_name']}",
+            user["iban"],
+            id_,
+        )
         return await database.fetch_one(complaint.select().where(complaint.c.id == id_))
 
     @staticmethod
@@ -58,3 +66,28 @@ class ComplaintManager:
             .where(complaint.c.id == complaint_id)
             .values(status=State.rejected)
         )
+
+
+    @staticmethod
+    async def issue_transaction(amount, full_name, iban, complaint_id):
+
+        wise = WiseService()
+        # print(wise.profile_id)
+        quote_id = wise.create_quote(amount)
+        recipient_id = wise.create_recipient_account(full_name, iban)
+        transfer_id = wise.transfer(recipient_id, quote_id)
+        #wise.fund_transfer(transfer_id)
+        # result = wise.fund_transfer(transfer_id)
+        # transaction_id = result['balanceTransactionId']
+        # status = result['status']
+        # print(status, transaction_id)
+        data = {
+            "quote_id": quote_id,
+            "target_account_id": str(recipient_id),
+            "transfer_id": transfer_id,
+            "amount": amount,
+            "complaint_id": complaint_id,
+        }
+        
+        await database.execute(transaction.insert().values(**data))
+        
---
+++ services/wise.py
@@ -0,0 +1,89 @@
+import json
+import os
+import uuid
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+
+wise_token = os.getenv("WISE_TOKEN")
+wise_url = os.getenv("WISE_URL")
+
+
+class WiseService:
+    def __init__(self):
+        self.main_url = wise_url
+        self.headers = {
+            "Content-type": "application/json",
+            "Authorization": f"Bearer {wise_token}",
+        }
+
+        self.profile_id = self._get_profile_id()
+
+    #       print(self.profile_id)
+
+    def _get_profile_id(self):
+        url = f"{self.main_url}/v2/profiles"
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            profile_id = response.json()[0]["id"]
+            return profile_id
+        else:
+            raise Exception("Could not get profile id")
+
+    def create_quote(self, amount):
+        url = f"{self.main_url}/v2/quotes"
+        data = {
+            "sourceCurrency": "EUR",
+            "targetCurrency": "EUR",
+            "sourceAmount": amount,
+            "profile": self.profile_id,
+        }
+        response = requests.post(url, headers=self.headers, json=data)
+        if response.status_code == 200:
+            return response.json()["id"]
+
+    def create_recipient_account(self, full_name, iban):
+        url = f"{self.main_url}/v1/accounts"
+        data = {
+            "currency": "EUR",
+            "type": "iban",
+            "profile": self.profile_id,
+            "accountHolderName": full_name,
+            "legalType": "PRIVATE",
+            "details": {"iban": iban},
+        }
+        response = requests.post(url, headers=self.headers, json=data)
+        if response.status_code == 200:
+            return response.json()["id"]
+        else:
+            raise Exception("Could not create recipient acccount")
+
+    def transfer(self, target_account_id, quote_id):
+        url = f"{self.main_url}/v1/transfers"
+        customer_transaction_id = str(uuid.uuid4())
+        data = {
+            "targetAccount": target_account_id,
+            "quoteUuid": quote_id,
+            "customerTransactionId": customer_transaction_id,
+            "details": {"reference": "Transfer to account"},
+        }
+        response = requests.post(url, headers=self.headers, json=data)
+        if response.status_code == 200:
+            # print(response.json())
+            #return True
+            return response.json()["id"]
+        else:
+            raise Exception("Could not transfer")
+
+    def fund_transfer(self, transfer_id):
+        url = f"{self.main_url}/v3/profiles/{self.profile_id}/transfers/{transfer_id}/payments"
+        data = {"type": "BALANCE"}
+        response = requests.post(url, headers=self.headers, json=data)
+        if response.status_code == 201:
+            #print(response.content)
+            return json.loads(response.content)
+        else:
+            raise Exception("Could not transfer")
+
+
